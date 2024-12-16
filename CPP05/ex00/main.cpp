b342a2528e1a2bbcb4341b1a313b0116da384a2c
Sahra Taskinen <sataskin@student.hive.fi>
2024-12-16 11:41:53 +0200

    done with CPP05 ex00

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,12 +6,63 @@
 /*   By: sataskin <sataskin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 12:23:51 by sataskin          #+#    #+#             */
-/*   Updated: 2024/12/14 12:24:12 by sataskin         ###   ########.fr       */
+/*   Updated: 2024/12/16 11:41:00 by sataskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main() {
+	try {
+		std::cout << "-> Making a Bureaucrat and incrementing and decrementing it" << std::endl;
+		Bureaucrat Burry;
+		std::cout << Burry << "\n-> will increment" << std::endl;
+		Burry.incrementGrade();
+		std::cout << Burry << "\n-> will decrement" << std::endl;
+		Burry.decrementGrade();
+		std::cout << Burry << std::endl;
+		std::cout << "->Now I'm going to decrement below the lowest level\n";
+		Burry.decrementGrade();
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try {
+		std::cout << "\n\n\n" << "-> Making a bureaucrat with input" << std::endl;
+		Bureaucrat Burton("Burton", 2);
+		std::cout << Burton << "\n-> will increment" << std::endl;
+		Burton.incrementGrade();
+		std::cout << Burton << "\n-> will decrement" << std::endl;
+		Burton.decrementGrade();
+		std::cout << Burton << std::endl;
+		std::cout << "-> Now I'm going to increment above the highest level\n";
+		Burton.incrementGrade();
+		Burton.incrementGrade();
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	
-}
+
+	try {
+		std::cout << "\n\n\n-> I will now make a Bureaucrat that is above highest level\n";
+		Bureaucrat Bur("Higih", 0);
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	try {
+		std::cout << "\n\n\n-> I will now make a Bureaucrat that is below lowest level\n";
+		Bureaucrat Bur("lowi", 166);
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
+}	
